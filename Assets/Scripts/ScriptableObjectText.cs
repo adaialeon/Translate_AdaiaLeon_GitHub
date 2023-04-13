095fb2de2ac67681a15f83e5b3f64e12b3accc54
Adaia León <95858353+adaialeon@users.noreply.github.com>
2023-04-13 23:52:52 +0200

    Entrega

---
+++ Assets/Scripts/ScriptableObjectText.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Translate", menuName = "Translate/New Translate")]
+
+public class ScriptableObjectText : ScriptableObject
+{
+    public string text1;
+    public string text2;
+    public string text3;
+    public string text4;
+}
